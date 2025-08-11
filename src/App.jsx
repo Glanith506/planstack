3e3ecbf707f68c505322a5addee7dc8d9aa95ded
Glanith506 <dcunhaglanith@gmail.com>
2025-08-11 09:11:52 +0530

    `Added React Router and related dependencies, updated package files and App component to use routing.`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
+import Home from "./pages/Home";
+import List from "./pages/ListAndEdit";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Router>
+      {/* my logo and navbar */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/pages/ListAndEdit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListAndEdit = () => {
+  return (
+    <div>
+      <h1>List and Edit</h1>
+    </div>
+  )
+}
+
+export default ListAndEdit
